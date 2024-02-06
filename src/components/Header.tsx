bc294eebf8b324b60d681bfab565fcc634d81d32
Jeffrey-mu <1115378579@qq.com>
2024-02-06 18:55:17 +0800

    wip: 优化header footer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,59 @@
-import { NavLink } from "react-router-dom";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+  Avatar,
+} from "@nextui-org/react";
 import Logo from "./svg/Logo";
-export default function Header() {
+import ThemeToggle from "./ThemeToggle";
+import { appConfig } from "@/lib/constant";
+import { NavLink } from "react-router-dom";
+export default function App() {
   return (
-    <div className="min-h-16 border-b-2 dark:bg-white/60 flex items-center">
-      <div className="max_width flex justify-between">
-        <NavLink to="/" className="flex items-center gap-2">
-          <Logo /> <h2 className="font-black text-2xl">文件校验</h2>
+    <Navbar isBordered maxWidth="2xl">
+      <NavbarBrand>
+        <NavLink to="/" className="flex gap-2">
+          <Logo />
+          <p className="font-bold text-inherit">{appConfig.siteName}</p>
         </NavLink>
-      </div>
-    </div>
+      </NavbarBrand>
+
+      <NavbarContent as="div" justify="end">
+        <ThemeToggle />
+
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
   );
 }
